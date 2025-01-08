888fd8192ad0d04b7947328825ae233774f91a5f
Elgara5364 <steveloardi5364@gmail.com>
2025-01-08 17:29:50 +0900

    fix fetch texture png

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
+  safelist: [
+    "bg-[url('https://elgara5364.github.io/wedding-EnEv/img/texture.jpg')]",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        texture:
+          "url('https://elgara5364.github.io/wedding-EnEv/img/texture.jpg')",
+      },
+    },
   },
   plugins: [],
 };
